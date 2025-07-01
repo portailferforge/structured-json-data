21837a0a4f07d47aaa86bf6843857fe10171e0b2
portailferforge <portails.ferforge@gmail.com>
2025-07-01 16:24:43 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "transferts-v1";
+const urlsToCache = [
+  "/",
+  "/?utm_source=pwa-fr",
+  "/images/favico/favicon-32x32.png",
+  "/images/favico/android-icon-192x192.png",
+  "/images/favico/apple-icon-180x180.png"
+];
+
+// Installation du service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
